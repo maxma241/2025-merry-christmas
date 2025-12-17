68556fe17157e64804782f780376320b07394228
Max Ma <marfi241@gmail.com>
2025-12-17 18:01:48 +0000

    chore: build base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
 
 export default defineConfig({
+  base: '/2025-merry-christmas/',
   plugins: [vue(), UnoCSS()],
   server: {
     host: true
